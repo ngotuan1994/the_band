82a4e47f237726db6926d96e2b70b4e60f00eab8
Eric Ngo <ngotuan1994@csu.fullerton.edu>
2021-08-05 18:46:47 -0700

    Mobile + Tablet

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const buyTickets = document.querySelectorAll(".js-buyTicket");
 const closeModal = document.querySelector(".modal-close");
 const modal = document.querySelector(".modal")
 const modalContainer = document.querySelector(".modal-container");
+const listMenu = document.querySelector(".nav_list");
+const navBarLeft = document.querySelector(".navigation_bar__left");
+const header = document.querySelector("#header")
 function showBuyTickets(){
     document.querySelector(".modal").classList.add("display_flex");
 }
@@ -26,10 +29,26 @@ function listenModalaContainer() {
         event.stopPropagation();
     })
 }
+function listenListMenu(){
+    
+    listMenu.addEventListener("click", ()=>{
+        if(navBarLeft.clientHeight > 200){
+            navBarLeft.style.height = "46px";
+            header.style.height = "46px";            
+        }
+        else{
+            navBarLeft.style.height = "230px";
+            header.style.height = "230px";
+        }
+                 
+       
+    })
+}
 listenBtnTicket();
 listenBtnCloseModal();
 listenModal();
 listenModalaContainer();
+listenListMenu();
 
 
 
